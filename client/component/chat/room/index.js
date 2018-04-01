13da1ad0b68bdc4a96f24319a7994d8652a5ba85
clear329 <clearg1929@gmail.com>
2018-04-01 16:13:03 +0200

    display created date of message

--- client/component/chat/room/index.js
+++ client/component/chat/room/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 export default class Room extends Component {
   constructor(props) {
@@ -17,13 +18,36 @@ export default class Room extends Component {
     this.el.scrollIntoView({ behavior: 'smooth' });
   }
 
+  calDuration(creatAt) {
+    var now = moment(new Date().toISOString()); //todays date
+    var end = moment(moment.unix(creatAt).format()); // another date
+    var duration = moment.duration(now.diff(end));
+    var days = duration.asDays();
+
+    if (Math.round(days) == 0) {
+      return "Today";
+    } else if (Math.round(days) == 1) {
+      return "Yesterday";
+    } else {
+      return moment.unix(creatAt).format('dddd, MMMM DD');
+    }
+  }
+
   render() {
     const { messages } = this.props;
+    let date, isDisplayDate = false;
     return (
       <div className="col-xs-8 col-sm-10 col-sm-push-2 main-chat-room">
         {messages.map(((msg, key) => {
+          if (date === moment.unix(msg.creatAt).format('dddd, MMMM DD, YYYY')) {
+            isDisplayDate = false;
+          } else {
+            date = moment.unix(msg.creatAt).format('dddd, MMMM DD, YYYY');
+            isDisplayDate = true;
+          }
           return (
             <div className="row" key={key}>
+              {isDisplayDate ? (<div className="col-md-12"><hr /><span>{this.calDuration(msg.creatAt)}</span><hr /></div>) : null}
               <div className="col-md-11" className="left-message">
                 <p>
                   <strong>{msg.userName}</strong><br />
